f30b285a36600f8950d1448868f7a8dea162c5d0
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2022-06-03 19:01:15 -0300

    Enhance default configurations Add .vscode folder to .gitignore

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,16 @@ use sdl2::pixels::Color;
 
 pub fn main() {
     let mut application = app::App::new(
-        (800, 600),
-        80,
-        Color::RGB(230, 230, 230),
+        (1200, 700),
+        50,
+        Color::RGB(230, 245, 245),
         Color::RGB(64, 64, 64),
-        (5, 4),
+        (10, 8),
         Color::RGB(0, 0, 0),
         &[
             Player { initial: 'C', color: Color::GREEN},
             Player { initial: 'M', color: Color::BLUE},
+            Player { initial: 'Y', color: Color::RGB(255, 127, 127)}
         ],
     );
     application.init();
