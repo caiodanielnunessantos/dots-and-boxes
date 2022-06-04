fd9765cb117e6e0d5e9c50cde1651ad85c8671b7
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2022-06-04 09:02:11 -0300

    Add html color names

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
 mod logic;
 mod app;
+mod colors;
 
 use app::Player;
-use sdl2::pixels::Color;
+use sdl2::{pixels::Color};
 
 pub fn main() {
     let mut application = app::App::new(
         (1200, 700),
         50,
-        Color::RGB(230, 245, 245),
-        Color::RGB(64, 64, 64),
+        colors::LightGray,
+        colors::Black,
         (10, 8),
-        Color::RGB(0, 0, 0),
+        colors::Black,
         &[
             Player { initial: 'C', color: Color::GREEN},
             Player { initial: 'M', color: Color::BLUE},
-            Player { initial: 'Y', color: Color::RGB(255, 127, 127)}
+            Player { initial: 'Y', color: colors::LightPink}
         ],
     );
     application.init();
